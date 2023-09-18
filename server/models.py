7e5298ffd44c82325ba88fd1ff1c19f0657157e1
Mohamed Hussein <mohamedmhussein@protonmail.com>
2023-09-17 22:38:33 -0400

    done

--- server/models.py
+++ server/models.py
@@ -12,6 +12,17 @@ class Author(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
+    @validates('name')
+    def validate_name(self, key, name):
+        if not name:
+            raise ValueError("Auther has to have a name")
+        return name
+    @validates('phone_number')
+    def validate_number(self, key, number):
+        if len(number) != 10:
+            raise ValueError("Phone number has to be 10 digits")
+        return number
+
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
 
@@ -27,6 +38,29 @@ class Post(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
+    @validates('content')
+    def validate_content(self, key, content):
+        if len(content) < 250:
+            raise ValueError("Content has to be more than 250 char")
+        return content
+    
+    @validates('summary')
+    def validate_summary(self, key, summary):
+        if len(summary) > 250:
+            raise ValueError("Summary has to be less than 250 char")
+        return summary
+    
+    @validates('category')
+    def validate_category(self, key, category):
+        if category not in ["Fiction", "Non-Fiction"]:
+            raise ValueError("Category must be Fiction or Non-Fiction")
+        return category
+
+    @validates('title')
+    def validate_title(self, key, title):
+        if title not in ["Won't Believe", "Secret","Top", "Guess"]:
+            raise ValueError("title is not clickbaitable")
+        return title
 
     def __repr__(self):
         return f'Post(id={self.id}, title={self.title} content={self.content}, summary={self.summary})'
